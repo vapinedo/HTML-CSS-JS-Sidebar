700d86a05a255c77ede45a1d59c9de0edc0a3437
Victor Pinedo <victorpinedomartinez@gmail.com>
2018-12-05 13:30:21 -0500

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+
+var toggleSidebar = document.querySelector('.toggle-sidebar');
+
+toggleSidebar.addEventListener('click', function() {
+  document.getElementById('sidebar').classList.toggle('collapsed');
+});
